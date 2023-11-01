f88c3992c01baa9387aeb8ad01725461995854b1
Ali.H <a.hashemi6336@gmail.com>
2023-11-01 21:32:01 +0330

    Add breadcrumb component

--- Frontend/src/Pages/CourseInfo/CourseInfo.jsx
+++ Frontend/src/Pages/CourseInfo/CourseInfo.jsx
@@ -1,10 +1,28 @@
 import "./CourseInfo.css";
+import Topbar from "../../Components/TopBar/Topbar";
+import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/Footer";
+import Breadcrumb from "../../Components/Breadcrumb/Breadcrumb";
 
 const CourseInfo = () => {
   return (
-    <div>
-      <p>CourseInfo</p>
-    </div>
+    <>
+      <Topbar />
+      <Navbar />
+      <Breadcrumb
+        links={[
+          { id: 1, title: "خانه", to: "/" },
+          { id: 2, title: "آموزش فرانت اند", to: "/category-info/frontend" },
+          {
+            id: 3,
+            title: "دوره متخصص جاوا اسکریپت",
+            to: "/course-info/js-expert",
+          },
+        ]}
+      />
+
+      <Footer />
+    </>
   );
 };
 
